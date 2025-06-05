3a34cfd3a898baebe4c89f86398fb6f6f6b11d05
ウエムー <7414847+uemue@users.noreply.github.com>
2025-06-05 23:52:21 +0900

    Import act at top of App test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import { render, screen, act } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 
@@ -24,4 +25,30 @@ describe('App', () => {
     render(<App />);
     expect(screen.getByRole('heading', { name: /advanced todo list/i })).toBeInTheDocument();
   });
+});
+
+
+
+it('shows remaining time when marking a completed task as incomplete', async () => {
+  jest.useFakeTimers();
+  jest.setSystemTime(new Date('2021-01-01T00:00:00Z'));
+  const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+
+  const { findByRole, getByRole, findByText } = render(<App />);
+
+  await user.type(getByRole('textbox', { name: /task description/i }), 'Test task');
+  await user.type(getByRole('spinbutton', { name: /estimated duration in minutes/i }), '1');
+  await user.click(getByRole('button', { name: /add task/i }));
+
+  await user.click(getByRole('button', { name: /start timer/i }));
+  await act(() => {
+    jest.advanceTimersByTime(5000);
+  });
+
+  await user.click(getByRole('button', { name: /mark task as complete/i }));
+  await findByText('00:05');
+
+  await user.click(getByRole('button', { name: /mark task as incomplete/i }));
+  await findByText('00:55');
+  jest.useRealTimers();
 });
